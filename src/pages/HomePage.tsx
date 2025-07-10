90385feaf265112d8108d771ef9668d9ee9e01e7
kkgomez2 <kkristoffergomez@gmail.com>
2025-07-10 10:12:22 -0700

    Talking to our API finally

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,62 @@
-type Props = {}
+import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import RateLimited from "../components/RateLimited";
+import toast from "react-hot-toast";
+
+type Props = {};
 
 const HomePage = (props: Props) => {
+  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchNotes = async () => {
+    try {
+      const res = await fetch("http://localhost:3001/api/notes", {
+        method: "GET",
+        mode: "cors",
+      });
+
+      if (res.ok) {
+        const data = await res.json();
+        setNotes(data);
+        setIsRateLimited(false);
+      }
+
+      if (res.status === 429) {
+        setIsRateLimited(true);
+        throw Response.error();
+      }
+    } catch (err) {
+      console.error("Error fetching notes: ", err);
+      toast.error("Failed to load notes");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
   return (
     <>
-    <div>HomePage</div>
-    <button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>
-    </>
-  )
-}
+      <NavBar />
 
-export default HomePage
+      {isRateLimited && <RateLimited />}
+
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+        {notes.length > 0 && !isRateLimited && (
+          <div>
+            {notes.map((note: {id: string, title: string}) => {
+              return (<div key={note.id}>Hello {note.title}</div>)
+            })}
+            </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
